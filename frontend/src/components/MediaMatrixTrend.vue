3e198f76f70d3481c15bf1970f24949b05bdc9c3
corparter <wujh2117@mails.jlu.edu.cn>
2023-08-20 18:14:14 +0800

    media matrix trend reordering and brushing

--- frontend/src/components/MediaMatrixTrend.vue
+++ frontend/src/components/MediaMatrixTrend.vue
@@ -82,8 +82,8 @@ export default {
                 col_labels.push("" + (i + 1));
             }
 
-            console.log(row_labels);
-            console.log(col_labels);
+            // console.log(row_labels);
+            // console.log(col_labels);
 
             var transpose = reorder.transpose(matrix),
                 dist_rows = reorder.dist()(matrix),
@@ -94,8 +94,8 @@ export default {
             
 
             let row_inv = reorder.inverse_permutation(row_perm);
-            // let col_inv = reorder.inverse_permutation(col_perm);
-            console.log(row_inv);
+            let col_inv = reorder.inverse_permutation(col_perm);
+            // console.log(row_inv);
             // console.log(col_inv);
             // console.log(matrix);
             // console.log(vdata);
@@ -113,8 +113,11 @@ export default {
             let rectWH = 10;
             let m = ({ l: 95, r: 25, t: 6, b: 35 });            
             let x = d3.scaleTime().range([m.l, width - m.r]).domain(d3.extent(xdata, d => new Date(d)));
-            let y = d3.scaleLinear()
-                .domain(d3.extent(ydata, d => +d))
+            // let y = d3.scaleLinear()
+            //     .domain(d3.extent(ydata, d => +d))
+            //     .range([m.t, height - m.b]);
+            let y = d3.scaleBand()
+                .domain(ydata)
                 .range([m.t, height - m.b]);
             
             let xAxis = g => g.append('g')
@@ -122,7 +125,7 @@ export default {
                 .call(d3.axisBottom(x))
 
             let yAxis = g => g.append('g')
-                .attr('transform', `translate(${m.l}, 0)`)
+                .attr('transform', `translate(${m.l}, ${0})`)
                 .call(d3.axisLeft(y).ticks(20))
 
             d3.select(self.$el).select('svg').remove();
@@ -155,7 +158,7 @@ export default {
                 .data(data1.values)
                 .join('g')
                 .attr('class', 'ggg')
-                .attr('transform', d=>`translate(${0}, ${y(d.topic) - 5 / 2})`)
+                .attr('transform', d=>`translate(${0}, ${y(d.topic)})`)
                 .selectAll('rect')
                 .data(d=>d.details)
                 .join('rect')
@@ -168,25 +171,34 @@ export default {
                     else if((d.value < 0)) return computeColorNeg(linearVDataNeg(d.value));
                     else return 'white';
                 })
-                // .attr("fill", d=> d.value != 0 ? 'steelblue' : 'none')
-                // .attr("stroke", 'steelblue')
             
             charts.append('title')
                 .text(d=>`from ${d.date0} to ${d.date1}, avgtone: ${d.value}`);
             
-
+            
+            var topicLocation = {}; // label i-th topic's location index
+            var new_ydate = [];
             var t = svg.transition().duration(1000);
             t.selectAll(".ggg")
                 .attr("transform", function(d, i) {
-                    return "translate(0," + (y((row_inv[i] + 1).toString()) - 5/2) + ")"; 
+                    // console.log(i,'---',row_inv[i]+1);
+                    topicLocation[row_inv[i]+1] = i + 1;
+                    return "translate(0," + y((row_inv[i]+1).toString()) + ")"; 
                 })
             
             t.selectAll(".yG").selectAll("text")
                 .text((d,i)=> {
-                    return getTopic((row_inv[i] + 1).toString())});
+                    return getTopic(topicLocation[i+1].toString())});
+                
+            // console.log(topicLocation);
+            new_ydate = Object.values(topicLocation).map(ele=>ele.toString())
+            // console.log(new_ydate);
+            let new_y = d3.scaleBand()
+                .domain(new_ydate)
+                .range([m.t, height - m.b]);
 
             let brush = d3.brush()
-                .extent([[0, 0], [width, height]])
+                .extent([[m.l, m.t], [width, height]])
                 .on('start brush', brushed)
                 .on('end', d=>{ // ending brush, gain storytree and draw stroytree
                     sysDatasetObj.updateMediaMatrixSelected(self.selected);
@@ -211,10 +223,12 @@ export default {
                     self.selected = {'topics': new Set(), 'date_index': new Set(), 'date': new Set()};
 
                     charts.attr('stroke', (d,i) =>{ // i represents the i-th time range index
+                        // console.log(topicLocation)
+
                         if(minX <= x(new Date(d.date0)) 
                             && x(new Date(d.date0)) <= maxX
-                            && minY <= y(d.topic)
-                            && y(d.topic) <= maxY 
+                            && minY <= new_y(d.topic)
+                            && new_y(d.topic) <= maxY
                         ){
                             self.selected['topics'].add(d.topic);
                             self.selected['date_index'].add(i);
